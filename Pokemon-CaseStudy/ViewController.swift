a03df50512a6255115616154b8552087e56d2c88
Hasan Dağ <daghsnn@gmail.com>
2022-11-03 11:33:56 +0300

    Initial Commit

---
+++ Pokemon-CaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon-CaseStudy
+//
+//  Created by Hasan Dag on 3.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
